346da5a7837521078012e09d49a863eda249139f
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-11-10 17:26:38 -0500

    feat: added logic for request methods

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -60,8 +60,17 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const reviews = books[isbn].reviews;
+  const newReview = req.body.review;
+
+  if(newReview) {
+    reviews[reviews.length] = newReview;
+    books[isbn].reviews = reviews;
+    return res.status(200).json({message: "Review added successfully"});
+  } else {
+    return res.status(400).json({message: "Review not added"});
+  }
 });
 
 module.exports.authenticated = regd_users;
